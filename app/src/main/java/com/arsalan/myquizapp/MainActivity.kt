d9a3af51cd7969ae209cd72eb180ea04cee27f00
Arsalan <arsalan.rk8@gmail.com>
2022-04-01 19:58:43 +0530

    first

---
+++ app/src/main/java/com/arsalan/myquizapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.arsalan.myquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
